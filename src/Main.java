70be0d880a3b04eb9d3e763d6bdced0769371f72
Luisantz <nnando52@gmail.com>
2026-03-05 18:01:50 -0300

    Adcionando projeto

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+void main() {
+    var scanner = new Scanner(System.in);
+
+    //Solicita o nome e a idade da primeira pessoa:
+    System.out.println("Digite aqui o nome da primeira pessoa:");
+    var name1 = scanner.nextLine();
+    System.out.println("Digite aqui o idade da primeira pessoa:");
+    var idade1 = scanner.nextInt();
+
+    //Limpa o buffer
+    scanner.nextLine();
+
+    //Solicita o nome e a idade da segunda pessoa:
+    System.out.println("Digite aqui o nome da segunda pessoa:");
+    var name2 = scanner.nextLine();
+    System.out.println("Digite aqui a idade da segunda pessoa:");
+    var idade2 = scanner.nextInt();
+
+    //Imprime a diferença de idade entre as duas pessoas:
+    var diferenca = idade1 - idade2;
+    System.out.printf("Essa é a diferença de idade entre %s e %s: %s ano(s) \n", name1, name2, diferenca);
 }
